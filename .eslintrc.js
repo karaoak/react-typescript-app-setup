452c9bf28c314fc86d78246e3c11c9d9d6780f4c
Frank Eijking <frank@medianique.nl>
2023-03-13 18:42:13 +0100

    Advanced ESLint plugin import/order setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'warn',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/no-default-export': 'warn',
+    'no-console': 'warn',
   },
 };
